32d466f5de7715d0b394f3be746a16578d522244
Christoph Matscheko <christoph.matscheko@gmx.net>
2017-10-21 17:10:52 +0200

    Navigate back to player selection after settting score

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+function getUrlParams() {
+	if (!window.location.search) {
+		return {}
+	}
+	return window.location.search
+		.substring(1)
+		.split("&")
+		.map((keyValueStr) => keyValueStr.split("="))
+		.reduce((urlParams, keyValuePair) => {
+			urlParams[keyValuePair[0]] = keyValuePair[1];
+			return urlParams;
+		}, {});
+}
 
