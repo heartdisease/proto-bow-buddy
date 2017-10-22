72c67e1b191ceee1d4aa3f238c3a1a133b58abbd
Christoph Matscheko <christoph.matscheko@gmx.net>
2017-10-22 11:44:48 +0200

    Integrate full-screen mode into all views

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-function getUrlParams() {
-	if (!window.location.search) {
-		return {}
+window.BowBuddy = window.BowBuddy || {
+	clickCounter: 0,
+
+	getUrlParams: function() {
+		if (!window.location.search) {
+			return {}
+		}
+		return window.location.search
+			.substring(1)
+			.split("&")
+			.map((keyValueStr) => keyValueStr.split("="))
+			.reduce((urlParams, keyValuePair) => {
+				urlParams[keyValuePair[0]] = keyValuePair[1];
+				return urlParams;
+			}, {});
+	},
+
+	switchToFullscreen: function() {
+		if ((++BowBuddy.clickCounter%2 === 0) & !document.fullscreenElement) {
+			console.log("Init fullscreen...");
+		
+			if (document.documentElement.requestFullscreen) {
+				document.documentElement.requestFullscreen();
+			} else if (document.documentElement.webkitRequestFullscreen) {
+				document.documentElement.webkitRequestFullscreen();
+			}
+		}
 	}
-	return window.location.search
-		.substring(1)
-		.split("&")
-		.map((keyValueStr) => keyValueStr.split("="))
-		.reduce((urlParams, keyValuePair) => {
-			urlParams[keyValuePair[0]] = keyValuePair[1];
-			return urlParams;
-		}, {});
-}
+};
 
