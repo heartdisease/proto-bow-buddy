3e6f6d2ec7e468867592c9e02743421d27cdf6c7
Christoph Matscheko <christoph.matscheko@gmx.net>
2018-10-26 21:17:23 +0200

    Change TypeScript compile target to ES6

--- src/main.ts
+++ src/main.ts
@@ -60,7 +60,7 @@ namespace BowBuddy {
   }
 
   export class Application {
-    private static /*final*/ VERSION = '2.0.5';
+    private static /*final*/ VERSION = '2.0.6';
     private static storage: DbAccess = null;
     private static currentView: BaseView = null;
 
