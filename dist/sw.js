e1538450a69a7caf3ee379979eafd9988d03fdaa
Christoph Matscheko <christoph.matscheko@gmx.net>
2019-03-09 17:43:27 +0100

    Remove usages of deprecated APIs from service worker

--- dist/sw.js
+++ dist/sw.js
@@ -3,13 +3,6 @@
 // 'https://fonts.gstatic.com/s/materialicons/v43/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 
 self.addEventListener('install', event => {
-  if ('registerForeignFetch' in event) {
-    event.registerForeignFetch({
-      scopes: self.registration.scope,
-      origins: ['https://fonts.googleapis.com', 'https://fonts.gstatic.com']
-    });
-  }
-
   event.waitUntil(
     caches.open('proto-bow-buddy').then(cache => {
       return cache.addAll([
@@ -36,26 +29,6 @@ self.addEventListener('fetch', event => {
       }
       console.log('Cache miss, need to load resource from server. (' + event.request.url + ')');
       return fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('foreignfetch', event => {
-  console.log('Fetching foreign resource: ' + event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        console.log('Use resource from cache. (' + event.request.url + ')');
-        return response;
-      }
-      console.log('Cache miss, need to load resource from server. (' + event.request.url + ')');
-      return fetch(event.request).then(response => {
-        return {
-          response,
-          origin: event.origin
-        };
-      });
     })
   );
 });
